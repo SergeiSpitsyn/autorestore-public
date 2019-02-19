33cdf325123254128c6df662f7222ee37e34311f
SerheoRayo <gerysubscribes@yandex.ru>
2019-02-20 01:05:23 +0300

    Try static files 4

--- autorestore/settings.py
+++ autorestore/settings.py
@@ -131,11 +131,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-# STATICFILES_DIRS = [
+STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static"),
-#     '/project/static/',
-#     '/webapps/django_autorestore/static/',
-# ]
+    '/project/static/',
+    '/webapps/django_autorestore/static/',
+]
 
 STATIC_URL = '/static/'
 
